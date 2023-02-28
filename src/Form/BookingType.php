8ae432f9ec8b8807eff862376f342f61f6812ed5
mirzalj1 <mirzalj908@gmail.com>
2023-02-28 09:18:13 +0100

    Updated forms and design od pages

--- src/Form/BookingType.php
+++ src/Form/BookingType.php
@@ -6,15 +6,19 @@ use App\Entity\Booking;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 class BookingType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
             ->add('booking_description')
-            ->add('booked_from')
-            ->add('booked_until')
+            ->add('booked_from', DateType::class, [
+                'widget' => 'single_text'
+            ])
+            ->add('booked_until', DateType::class, [
+                'widget' => 'single_text'
+            ])
             ->add('room')
         ;
     }
